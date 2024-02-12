0504886e5d28fbd25268811431c83e6f5bb8c08f
rajashri <rajashrilinge2004@gmail.com>
2024-02-12 20:17:24 +0530

    Layout2

---
+++ Css/Login form/index.js
@@ -0,0 +1,8 @@
+const h1 =document.querySelector("#head");
+function changetext() {
+    h1.innerText = "Sign in";
+}
+const input =document.querySelector("input");
+function changein() {
+    input.textContent = " user name ";
+}
