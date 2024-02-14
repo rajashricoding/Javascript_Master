2e8a56b3ca6f224bbe07deff0df6f919a972fdcc
rajashri <rajashrilinge2004@gmail.com>
2024-02-14 14:32:38 +0530

    design

--- Css/Login form/index.js
+++ Css/Login form/index.js
@@ -1,8 +1,11 @@
 const h1 =document.querySelector("#head");
-function changetext() {
+/*function changetext() {
     h1.innerText = "Sign in";
-}
-const input =document.querySelector("input");
+}*/
+const input =document.querySelector("#in");
+const label =document.querySelector("#label");
 function changein() {
-    input.textContent = " user name ";
+    input.setAttribute("placeholder", "User name");
+    label.innerText = "Enter user name";
+    h1.innerText = "Sign up";
 }
