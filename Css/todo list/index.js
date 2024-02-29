2b09af4effdc0c72f09ab8e518b01254816c3f8a
rajashri <rajashrilinge2004@gmail.com>
2024-02-29 13:11:57 +0530

    calculator layout

--- Css/todo list/index.js
+++ Css/todo list/index.js
@@ -1,17 +1,21 @@
 const input = document.querySelector("input");
 const add = document.querySelector(".add");
 const list = document.querySelector(".todo_items");
+
 add.addEventListener ("click", ( ) => {
     let inputValue = input.value;
-    if (inputValue.length == 0) {
+    if(inputValue.length == 0) {
         alert("Please Write Something");
     }
     else{
         let li= document.createElement("li");
         li.innerHTML =`<p> ${inputValue} </p> 
-        <button class="remove"> x </button>
-        `;
-        list.appendChild("li");
-        inputValue = " ";
+        <button class="remove">x</button>`;
+        list.appendChild(li);
+        input.value = " ";
+        let remove = li.querySelector(".remove");
+        remove.addEventListener ("click" , ( ) => {
+                list.removeChild(li);
+        });
     }
 });
