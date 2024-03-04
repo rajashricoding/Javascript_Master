a20be09adcc5d0b1e3d6e507bb9d9b59d0ddf1b5
rajashri <rajashrilinge2004@gmail.com>
2024-03-04 15:52:21 +0530

    raelstate

---
+++ Css/calculator/index.js
@@ -0,0 +1,7 @@
+const input= document.querySelector("input");
+const add= document.querySelector(".add");
+add.addEventListener("click" , ( ) => {
+    function add(value){
+        input.value += value;
+    }
+});
